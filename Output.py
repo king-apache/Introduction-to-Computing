fc489b06e0ff40f812cd94e6befbd79efef155a9
king-apache <97752486+king-apache@users.noreply.github.com>
2022-01-14 23:18:37 +0530

    Add files via upload

--- Output.py
+++ Output.py
@@ -4,6 +4,7 @@ Type "help", "copyright", "credits" or "license()" for more information.
 === RESTART: D:\Python\assignment.py ==
 
 
+                                                  #Question1
 
 Enter num 1
 4124
@@ -12,11 +13,23 @@ Enter num 2
 Enter num 3
 34647
 The average is  26698.333333333332
+
+
+ 
+
+                                                  #Question2
+
 Enter your Gross Income
 6799637
 Enter the number of Dependants
 2
 Tax =  1356727.4000000001
+
+
+
+
+                                                  #Question3
+
 Enter SID
 385796855
 Enter Name
@@ -28,6 +41,12 @@ Plebian
 Enter CGPA
 9
 [385796855, 'King Darius', 'Philosophie', 'Plebian', 9.0]
+
+
+
+
+                                                   #Question4 
+
 Enter 5 numbers
 856457
 3546737
@@ -35,6 +54,13 @@ Enter 5 numbers
 34537
 486573
 [3735, 34537, 486573, 856457, 3546737]
+
+
+
+
+
+                                                   #Question5   
+
 ['Red', 'Green', 'White', 'Pink', 'Yellow']
 ['Red', 'Green', 'White', 'Purple', 'Purple', 'Yellow']
 ['Red', 'Green', 'White', 'Purple', 'Yellow']
